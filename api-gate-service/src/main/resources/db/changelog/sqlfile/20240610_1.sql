c63a65d8886fb2e3c2722e27a4da670d805d9eba
emberow <chin45630@gmail.com>
2024-07-05 17:04:42 +0800

    feat: add spring cloud gateway example

--- api-gate-service/src/main/resources/db/changelog/sqlfile/20240610_1.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.users (
-	id serial4 NOT NULL,
-	"name" varchar NULL,
-	email varchar NULL,
-	CONSTRAINT users_pk PRIMARY KEY (id)
-);
--- api-gateway-service/src/test/java/com/gateway/ApiGatewayTests.java
+++ api-gateway-service/src/test/java/com/gateway/ApiGatewayTests.java
@@ -1,4 +1,4 @@
-package com.api.gateway;
+package com.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
