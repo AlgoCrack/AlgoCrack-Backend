2f0aa04e7733f1500ead118ca9500166e4f62bd5
emberow <chin45630@gmail.com>
2025-05-28 16:11:44 +0800

    feat: init nestjs project

--- problem-service/src/main/java/com/problem/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.problem.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
