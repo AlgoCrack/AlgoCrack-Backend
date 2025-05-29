795a0fc9edaaa132a1074a2b1e0f87fe7907847f
emberow <chin45630@gmail.com>
2025-05-29 10:51:01 +0800

    feat: add dotenv

--- problem-service/src/main.ts
+++ problem-service/src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
